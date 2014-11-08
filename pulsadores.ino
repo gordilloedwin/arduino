d31cd068573f91f442018b8489435c4a1cb9696a
Ed Cardoz <edgar.cardoz@gmail.com>
2014-11-08 00:40:23 -0600

    Creatre pulsadores.ino
    Primera version.
    Se utilizan 4 LEDs y dos pulsadores para recorrer los pines y encender los LEDs. Los pulsadores hacen cambiar la dirección del encendido de los LEDs.

---
+++ pulsadores.ino
@@ -0,0 +1,41 @@
+int pulsador=2; //  Primer pulsador
+int pulsador2=3;//  Segundo pulsador
+
+int pin[]={8,9,10,11}; // Array para recorrer LEDs con 
+                       // primer pulsador
+                       
+int pin2[]={11,10,9,8};// Array para recorrer LEDs con
+                       // segundo pulsador
+
+int i=0;    // Declaracion del index
+
+void setup(){
+  pinMode(pulsador,OUTPUT); //Configuracion pin pulsador
+  pinMode(pulsador2,OUTPUT);//Configuracion pin pulsador2
+  
+  for(i=0; i<4; i++){       //Configuracion de pines de
+    pinMode(pin[i], OUTPUT);//los LEDs
+  }
+}
+
+void loop(){
+  //Deteccion del primer pulsador
+  if(digitalRead(pulsador)==HIGH){    
+    for(i=0; i<4; i++){
+     digitalWrite(pin[i],HIGH);
+     delay(500);
+     digitalWrite(pin[i],LOW);
+     delay(100);
+   }
+  }
+  
+  //Deteccion del segundo pulsador
+  if(digitalRead(pulsador2)==HIGH){
+    for(i=0; i<4; i++){
+     digitalWrite(pin2[i],HIGH);
+     delay(500);
+     digitalWrite(pin2[i],LOW);
+     delay(100);
+   }
+  }
+}
